5f49574376f9f1894484e59ca64e4094ddef36ce
essenceD <64703800+essenceD@users.noreply.github.com>
2020-05-03 21:10:25 +0300

    Update pupils_hights.py

--- pupils_hights.py
+++ pupils_hights.py
@@ -1,15 +1,15 @@
-klass = {x: [] for x in range(1, 12)}
+_сlass = {x: [] for x in range(1, 12)}
 with open('c:\inputs\dataset_3380_5.txt', 'r') as file:
     for line in file:
         line = line.strip().split()
-        klass[int(line[0])].append(int(line[2]))
-for i, j in klass.items():
-    if klass[i] == []:
-        klass[i] = '-'
+        _сlass[int(line[0])].append(int(line[2]))
+for i, j in _сlass.items():
+    if _сlass[i] == []:
+        _сlass[i] = '-'
         continue
-    klass[i] = sum(klass[i]) / len(klass[i])
-for i, j in klass.items():
+    _сlass[i] = sum(_сlass[i]) / len(_сlass[i])
+for i, j in _сlass.items():
     print(i, j)
-with open('c:\outputs\klass.txt','w') as file:
-    for i, j in klass.items():
+with open('c:\outputs\сlass.txt','w') as file:
+    for i, j in _сlass.items():
         file.write('{} {}\n'.format(i, j))
