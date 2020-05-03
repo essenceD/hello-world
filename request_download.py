1a55000af3be3a3e71bf39842ad920885739f68c
essenceD <64703800+essenceD@users.noreply.github.com>
2020-05-03 21:12:48 +0300

    Update request_download.py

--- request_download.py
+++ request_download.py
@@ -1,12 +1,9 @@
 import requests
-n = 0
 with open('c:\inputs\dataset_3378_2.txt', 'r') as file:
     url = file.readline().strip()
 response = requests.get(url)
 with open('c:\inputs\content.txt', 'w') as file:
     file.write(response.text)
 print(len(response.text.splitlines()))
-#for i in response.text.splitlines():
-#    n += 1
 with open('c:\outputs\\answer.txt', 'w') as file:
     file.write(str(len(response.text.splitlines())))
